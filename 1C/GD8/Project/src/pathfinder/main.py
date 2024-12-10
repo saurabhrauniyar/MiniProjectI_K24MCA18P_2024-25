f915ddd30037417dff06bc08985afbe303280670
n1k1ta21 <nikita.2426mca686@kiet.edu>
2024-12-10 23:08:08 +0530

    Add files via upload

---
+++ 1C/GD8/Project/src/pathfinder/main.py
@@ -0,0 +1,32 @@
+import time
+from typing import Callable
+
+from .search.astar import AStarSearch
+from .search.gbfs import GreedyBestFirstSearch
+from .search.dijkstras import DijkstrasSearch
+from .models.grid import Grid
+from .models.solution import Solution
+from .models.search_types import Search
+from .models.search_types import Search
+
+SearchFunction = Callable[[Grid], Solution]
+
+SEARCH: dict[Search, SearchFunction] = {
+    Search.ASTAR_SEARCH: AStarSearch.search,
+    Search.DIJKSTRAS_SEARCH: DijkstrasSearch.search,
+    Search.GREEDY_BEST_FIRST_SEARCH: GreedyBestFirstSearch.search,
+}
+
+
+class PathFinder:
+    @staticmethod
+    def find_path(
+        grid: Grid,
+        search: Search,
+    ) -> Solution:
+        start_time = time.perf_counter()
+        solution = SEARCH[search](grid)
+        time_taken = (time.perf_counter() - start_time) * 1000
+        solution.time = time_taken
+
+        return solution
